ef3c89eae43749e1d5f9bd16f906421a02d8ded5
the-bay-kay <katie.risc@gmail.com>
2022-07-19 15:34:32 -0700

    Trawl funcitonal with toy dictionary
    Given an input dicitonary (words seperated by a newline), read
    each input file, grab the corresponding IPA from the cambridge
    dictionary, and then write it to an outfile
    
    This currently works with the toy dictionary (included in this
    commit), and I will now run it with the full linux.words!

--- ipa_trawler.sh
+++ ipa_trawler.sh
@@ -1,24 +1,30 @@
 #!/usr/bin/bash
 
-SAVEIFS=$IFS
-IFS=$'\n'
 # DICT_PATH="/usr/share/dict/linux.words"
 DICT_PATH="/home/katie/Documents/scripts/ipa_trawler/dict.txt"
 LINK="https://dictionary.cambridge.org/dictionary/english/"
-# Using /tmp/ rather than saving as a variable, as it makes the
-# process of stripping the IPA character easier 
-# wget --quiet $TEST -O '/tmp/testpen.html'
-echo 
+OUTFILE="US_English_IPA.dict"
+# Outfile setup
+touch $OUTFILE
+
 while read -r line;
 do
     url="${LINK}$line"
     f_path="/tmp/${line}-ipa_trawler.txt"
-    echo $f_path
     wget --quiet $url -O $f_path
+    err=$(cat $f_path | grep 'home')
+    # If dictionary entry doesn't exist, reroutes to homepag
+    if ! [ -z "$err" ];
+    then
+        >&2 echo "    [!] ${line} Not Found"
+        continue
+    fi
+    # This temp readline is janky, BUT IT WORKS B)
     tmp=$(cat $f_path | grep 'ipa' | { read l_1; read l_2; echo $l_2 ; })
-    echo $tmp
+    tmp=${tmp:68}
+    tmp=${tmp::-50}
+    output="${line} ${tmp}"
+    echo $output >> $OUTFILE
+    >&1 echo "${line}"
     rm $f_path
-    echo -e "$URL"
 done <$DICT_PATH
-
-IFS=$SAVEIFS
