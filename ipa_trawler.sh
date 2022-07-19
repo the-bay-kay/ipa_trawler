840eca70d7b2d9c664901e1ac27381d3417685a1
the-bay-kay <katie.risc@gmail.com>
2022-07-19 13:55:41 -0700

    Added basic scraping of HTML
    Using .words file specified by $DICT_PATH, take each word,
    wget the corresponding page from the website, and get the line
    of HTML corresponding to the US IPA transcription.
    
    TODO:
    Strip the IPA transcription and store it within an outfile

--- ipa_trawler.sh
+++ ipa_trawler.sh
@@ -1,13 +1,24 @@
 #!/usr/bin/bash
 
-DICT_PATH="/usr/share/dict/linux.words"
+SAVEIFS=$IFS
+IFS=$'\n'
+# DICT_PATH="/usr/share/dict/linux.words"
+DICT_PATH="/home/katie/Documents/scripts/ipa_trawler/dict.txt"
 LINK="https://dictionary.cambridge.org/dictionary/english/"
-TEST="${LINK}penguin"
-wget --quiet $TEST
-echo $TEST
-exit
+# Using /tmp/ rather than saving as a variable, as it makes the
+# process of stripping the IPA character easier 
+# wget --quiet $TEST -O '/tmp/testpen.html'
+echo 
 while read -r line;
 do
-    echo -e "$line"
+    url="${LINK}$line"
+    f_path="/tmp/${line}-ipa_trawler.txt"
+    echo $f_path
+    wget --quiet $url -O $f_path
+    tmp=$(cat $f_path | grep 'ipa' | { read l_1; read l_2; echo $l_2 ; })
+    echo $tmp
+    rm $f_path
+    echo -e "$URL"
 done <$DICT_PATH
 
+IFS=$SAVEIFS
